1fd7582dd38408593a1f7947e740718ed917f7c5
LissetteHuilca <lissette.huilca@gmail.com>
2019-06-10 23:30:00 -0500

    Add Assets

---
+++ Assets/Script/MinBeehive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinBeehive : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var otherObject = collision.collider.gameObject;
+        if (otherObject.tag == "Player")
+        {
+            var scale = this.transform.localScale;
+            scale.y = 0.2f;
+            scale.x = 0.2f;
+            this.transform.localScale = scale;
+
+            var position = this.transform.localPosition;
+            position.x = 5.5f;
+            this.transform.localPosition = position;
+        }
+    }
+}
